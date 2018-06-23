f91e969190c0b8ef3b39facd730243e94ffbb41f
fabian.winter01 <fabian.winter01@stud.uni-goettingen.de>
2018-06-23 14:25:09 +0200

    Merge MainBoard class from Board branch
    The MainBoard class (which makes DummyBoard obsolete) contains an implementation of Viewer, thus making BoardViewer obsolete as well.
    
    Closes #6

---
+++ src/flowerwarspp/board/MainBoard.java
@@ -0,0 +1,248 @@
+package flowerwarspp.board;
+
+import flowerwarspp.preset.*;
+
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+/*
+TODO: eigenen besseren Floweralgo benutzen
+*/
+
+/**
+ * Verwaltungsklasse, die Daten über die gemachten und noch möglichen Züge
+ * eines Spielers Speichert
+ */
+class PlayerData {
+	/**
+	 * Die Blumen, die der Spieler gesetzt hat.
+	 */
+	HashSet<Flower> flowers = new HashSet<>();
+	/**
+	 * Die Gräben, die der Spieler gesetzt hat.
+	 */
+	HashSet<Ditch> ditches = new HashSet<>();
+	/**
+	 * Die legalen Züge, die der Spieler noch machen kann.
+	 */
+	HashSet<Move> legalMoves = new HashSet<>();
+}
+
+/**
+ * Boardimplementation. Implementiert Board und somit auch Viewable
+ *
+ * @version 0.1
+ */
+public class MainBoard implements Board {
+	/**
+	 * Groesse des Boards
+	 */
+	private final int size;
+
+	/**
+	 * Der Spieler, der aktuell am Zug ist.
+	 */
+	private PlayerColor currentPlayer = PlayerColor.Red;
+
+	/**
+	 * Der Spieler, welcher aktuell nicht am Zug ist
+	 */
+	private PlayerColor oppositePlayer = PlayerColor.Blue; // Sonst macht man es x mal redundant beim checken
+
+	/**
+	 * Der Aktuelle Status des Spielbretts.
+	 */
+	private Status currentStatus = Status.Ok;
+
+	/**
+	 * Daten über die Spieler.
+	 */
+	private EnumMap<PlayerColor, PlayerData> playerData = new EnumMap<>(PlayerColor.class);
+
+
+	/**
+	 * Konstruktor. Befuellt Board einer variablen Groesse zwischen [3;30].
+	 * Falls Wert invalide wird dieser dem naechsten Element des Intervalls angepasst.
+	 *
+	 * @param size Groesse des Boardes.
+	 */
+	public MainBoard(final int size) {
+		this.size = (size < 3) ? 3 : ((size > 30) ? 30 : size);
+
+		playerData.put(PlayerColor.Red, new PlayerData());
+		playerData.put(PlayerColor.Blue, new PlayerData());
+
+		Flower[] flowers = new Flower[this.size * this.size];
+		int insertPosition = 0;
+		for (int i = 1; i <= this.size; i++) {
+			for (int j = 1; j <= this.size - (i - 1); j++) {
+				flowers[insertPosition] = (new Flower(
+						new Position(i, j),
+						new Position(i + 1, j),
+						new Position(i, j + 1)
+				));
+				insertPosition++;
+
+				if (i + j <= this.size) {
+					flowers[insertPosition] = (new Flower(
+							new Position(i + 1, j + 1),
+							new Position(i + 1, j),
+							new Position(i, j + 1)
+					));
+					insertPosition++;
+				}
+			}
+		}
+		for (int i = 0; i < flowers.length; i++) {
+			for (int j = i + 1; j < flowers.length; j++) {
+				Move move = new Move(flowers[i], flowers[j]);
+				playerData.get(PlayerColor.Red).legalMoves.add(move);
+				playerData.get(PlayerColor.Blue).legalMoves.add(move);
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * Verifiziert Zug und fuehrt diesen dann aus.
+	 *
+	 * @param move auszufuehrender Zug
+	 * @throws IllegalStateException Wenn Zug nicht valide ist.
+	 */
+	@Override
+	public void make(final Move move) throws IllegalStateException {
+		if (!playerData.get(currentPlayer).legalMoves.contains(move)) {
+			throw new IllegalStateException("Illegaler Zug");
+		}
+		// Ist es best practise nicht null zu checken weil es literally unmoeglich ist?
+		// (Falls es nicht so ist Kommentar einfach removen)
+		switch (move.getType()) {
+			case Ditch:
+				playerData.get(currentPlayer).ditches.add(move.getDitch());
+				updateValidMoves(move.getDitch());
+			case Flower:
+				updateValidMoves(new Flower[]{move.getFirstFlower(), move.getSecondFlower()});
+			case End:
+				// TODO
+				return;
+			case Surrender:
+				// TODO
+				return;
+		}
+		currentPlayer = oppositePlayer;
+		oppositePlayer = (currentPlayer == PlayerColor.Red) ? PlayerColor.Blue : PlayerColor.Red;
+	}
+
+	private void updateValidMoves(Flower[] fs) {
+        /*
+        Was aktuell gemacht wird: (als Referenz zum erweitern (Kommentar kommt bei Abgabe raus))
+            - Gaertencheck
+            - Exkludieren von Gaertenabstaenden
+            - Moves fuer andere Farbe exkludieren
+            - Grabenerstellung:
+                - Gegnerische Graeben entfernen falls geblockt durch eigene Blume
+                - Graben erlauben falls direkte Verbindung UND kein existierender Graben teilt (kann das der Fall sein nach Ditchchecks?)
+         */
+		// TODO: Ist es noetig zu checken ob Flower in valider Spielfeldrange ist?
+		// Idee: Gaerten einzeln speichern um Laufzeit zu verbessern da man nur Aussenbereiche testen muss und diese immutable sind
+		for (Flower f : fs) {
+			// Gesetzte Flowern als valider Zug fuer andere exkludieren
+			for (Flower oppositeF : playerData.get(oppositePlayer).flowers) {
+				Move move = new Move(f, oppositeF);
+				playerData.get(oppositePlayer).legalMoves.remove(move);
+			}
+
+			//if (flowerBedSize() == 4) {
+
+			//}
+		}
+	}
+
+	private int flowerBedSize(Flower f) {
+		// TODO
+		return 42;
+	}
+
+	private void updateValidMoves(Ditch d) {
+        /*
+        Was aktuell gemacht wird:
+            - Ueber und unter Graben Flower entvalidieren
+            - Andere Graebenmoeglichkeiten entvalidieren falls diese sich eine Position teilen
+         */
+		// Blumen entvalidieren
+
+	}
+
+	/**
+	 * Gibt den dazugehoerigen Viewer der Klasse {@link MainBoardViewer} zurueck.
+	 *
+	 * @return den dazugehoerigen Viewer
+	 */
+	@Override
+	public Viewer viewer() {
+		return new MainBoardViewer();
+	}
+
+	/**
+	 * Ein Viewer auf das MainBoard.
+	 */
+	private class MainBoardViewer implements Viewer {
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public HashSet<Ditch> getDitches(PlayerColor color) {
+			return new HashSet<>(playerData.get(color).ditches);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public HashSet<Flower> getFlowers(PlayerColor color) {
+			return new HashSet<>(playerData.get(color).flowers);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getPoints(PlayerColor color) {
+			// TODO!
+			return 0;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public HashSet<Move> getPossibleMoves() {
+			return new HashSet<>(playerData.get(currentPlayer).legalMoves);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public int getSize() {
+			return size;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public Status getStatus() {
+			return currentStatus;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public PlayerColor getTurn() {
+			return currentPlayer;
+		}
+	}
+}
--- src/flowerwarspp/io/BoardFrame.java
+++ src/flowerwarspp/io/BoardFrame.java
@@ -1,23 +1,27 @@
 package flowerwarspp.io;
 
-import flowerwarspp.preset.Board;
-import flowerwarspp.preset.Move;
-import flowerwarspp.preset.Requestable;
+import flowerwarspp.board.MainBoard;
+import flowerwarspp.preset.*;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import java.util.Collection;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class BoardFrame extends JFrame implements Requestable {
 	public static void main(String[] args) throws Exception {
-		DummyBoard dummyBoard = new DummyBoard(30);
 		BoardFrame boardFrame = new BoardFrame();
 		boardFrame.setVisible(true);
-		boardFrame.addMouseListener((MouseClickListener) e ->
-		        boardFrame.setBoard(dummyBoard));
 	}
+
+	private MainBoard mainBoard = new MainBoard(10);
+	private Viewer viewer = mainBoard.viewer();
 
 	private SpringLayout springLayout = new SpringLayout();
 	private JPanel container = new JPanel(this.springLayout);
+	private JButton testFlowerMove = new JButton("Random flower");
+	private JButton testDitchMove = new JButton("Random ditch");
 	private BoardDisplay boardDisplay = new BoardDisplay();
 
 	public BoardFrame() {
@@ -27,7 +31,34 @@ public class BoardFrame extends JFrame implements Requestable {
 
 		this.add(this.container);
 		this.container.add(this.boardDisplay);
+		this.container.add(this.testFlowerMove);
+		this.container.add(this.testDitchMove);
 		this.boardDisplay.updateSize();
+
+		this.boardDisplay.addMouseListener((MouseClickListener) e -> {
+			this.setBoard(mainBoard);
+			System.out.println("Hallo");
+		});
+
+		this.testFlowerMove.addMouseListener((MouseClickListener) e -> {
+			Collection<Move> moves = this.viewer.getPossibleMoves()
+			        .stream()
+			        .filter(t -> t.getType() == MoveType.Flower)
+			        .collect(Collectors.toList());
+
+			Random random = new Random();
+			int idx = random.nextInt(moves.size());
+			int i = 0;
+			for (Move m : moves) {
+				if (i == idx) {
+					System.out.println(m.getType());
+					this.mainBoard.make(m);
+					break;
+				}
+
+				i++;
+			}
+		});
 
 		// Call after adding all the components to the container.
 		// Sets up the layout of everything.
@@ -50,12 +81,29 @@ public class BoardFrame extends JFrame implements Requestable {
 	 * sollen.
 	 */
 	private void setupConstraints() {
+		// Lays out the display itself
 		this.springLayout.putConstraint(
 		        SpringLayout.HORIZONTAL_CENTER, this.boardDisplay, 0,
 		        SpringLayout.HORIZONTAL_CENTER, this.container);
 		this.springLayout.putConstraint(
 		        SpringLayout.VERTICAL_CENTER, this.boardDisplay, 0,
 		        SpringLayout.VERTICAL_CENTER, this.container);
+
+		// Lays out the flower move test button
+		this.springLayout.putConstraint(
+		        SpringLayout.SOUTH, this.testFlowerMove, 0,
+				SpringLayout.SOUTH, this.container);
+		this.springLayout.putConstraint(
+		        SpringLayout.WEST, this.testFlowerMove, 0,
+		        SpringLayout.WEST, this.container);
+
+		// Lays out the ditch move test button
+		this.springLayout.putConstraint(
+		        SpringLayout.SOUTH, this.testDitchMove, 0,
+		        SpringLayout.SOUTH, this.container);
+		this.springLayout.putConstraint(
+		        SpringLayout.WEST, this.testDitchMove, 0,
+		        SpringLayout.EAST, this.testFlowerMove);
 	}
 
 	public void setBoard(Board board) {
