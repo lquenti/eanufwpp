202f25ddbd9c4f1784853f4f1050d572a05b23fc
Mokka <mokkamichi.99@gmail.com>
2018-06-28 00:41:35 +0200

    Dokumentation komplettiert, Board implementiert
    Die Änderungen des board branches wurden integriert, die Dokumentation wurde komplettiert und angepasst.

--- src/flowerwarspp/player/BasePlayer.java
+++ src/flowerwarspp/player/BasePlayer.java
@@ -1,30 +1,60 @@
 package flowerwarspp.player;
 
 import flowerwarspp.board.MainBoard;
-import flowerwarspp.io.BoardViewer;
 import flowerwarspp.preset.*;
 
 import java.rmi.RemoteException;
-import java.util.Collection;
 
-
+/**
+ * Abstrakte Basis-Klasse welche die grundlegende Implementation eines Spielers beschreibt, welcher die Anforderungen
+ * des Interfaces {@link Player} erfuellt. Die einzige abstrakte Methode deren Implementation gefordert wird, ist {@link
+ * #requestMove()}. Diese Methode fordert einen Zug vom jeweiligen Spieler an, und leitet diesen Zug an die Methode
+ * {@link #request()} weiter.
+ *
+ * @author Mokka
+ */
 abstract class BasePlayer implements flowerwarspp.preset.Player {
 
-    static final String exception_NoInit =
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn der Spieler noch nicht
+     * initialisiert worden ist.
+     */
+    private static final String exception_NoInit =
             "Der Spieler muss zuerst initialisiert werden!";
 
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn eine der Methoden {@link
+     * #confirm(Status)}, {@link #request()} oder {@link #update(Move, Status)} zu einem unerwarteten Zeitpunkt
+     * aufgerufen worden wird.
+     */
     private static final String exception_UnexpectedCall =
             "Unerwarteter Methoden-Aufruf.";
 
-    static final String exception_CycleRequest =
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn {@link #request()} zum falschen
+     * Zeitpunkt aufgerufen worden ist.
+     */
+    private static final String exception_CycleRequest =
             exception_UnexpectedCall + " Es haette update() aufgerufen werden sollen.";
 
-    private  static final  String exception_CycleUpdate =
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn {@link #update(Move, Status)}
+     * zum falschen Zeitpunkt aufgerufen worden ist.
+     */
+    private static final String exception_CycleUpdate =
             exception_UnexpectedCall + " Es haette confirm() aufgerufen werden sollen.";
 
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn {@link #confirm(Status)} zum
+     * falschen Zeitpunkt aufgerufen worden ist.
+     */
     private static final String exception_CycleConfirm =
             exception_UnexpectedCall + " Es haette request() aufgerufen werden sollen.";
 
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn es eine Disparitaet zwischen den
+     * Status des Hauptprogramms und des eigenen Spielbretts gab.
+     */
     private static final String exception_StatusError =
             "Der Status des Hauptprogramms und der Status des Spielbretts dieses Spielers stimmen nicht ueberein!";
 
@@ -48,6 +78,12 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
     protected Board board;
 
     /**
+     * Ermoeglicht den Zugriff auf relevante Daten des Spielbretts, welche fuer die Verifikation und die Ausarbeitung
+     * von Spielzuegen benoetigt werden.
+     */
+    protected Viewer boardViewer;
+
+    /**
      * Die Farbe dieses Spielers, nach den Vorgaben des enums {@link PlayerColor}.
      */
     private PlayerColor playerColour;
@@ -56,12 +92,7 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
      * Wird genutzt, um den aktuellen Status des Spieler-Lebenszyklus (also der Zyklus, welcher im Interface {@link
      * flowerwarspp.preset.Player} diktiert wird) darzustellen.
      */
-    protected PlayerFunction cycleState;
-
-    /**
-     * Mit Hilfe des BoardViewer werden relevante und {@link Board} bezogene Daten angefordert und intern verwendet.
-     */
-    private BoardViewer viewer;
+    private PlayerFunction cycleState;
 
     /**
      * Ein <code>default</code>-Konstruktor, welcher die Instanzvariablen mit Basiswerten initialisiert.
@@ -73,11 +104,9 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
     }
 
 
-    /* INFO: Method is abstract because requesting a move from the player works differently with each implementation */
-
     /**
      * Methode zum Anfordern eines Zugs.
-     * 
+     *
      * @return Der vom Spieler geforderte Zug
      * @throws Exception       Falls der Spieler nicht in der Lage war, einen Zug zu liefern oder falls diese Methode
      *                         zum falschen Zeitpunkt innerhalb des Zyklus aufgerufen worden ist
@@ -86,8 +115,8 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
     @Override
     public Move request() throws Exception, RemoteException {
         // State validation
-        if ( this.getCycleState() == PlayerFunction.NULL ) throw new Exception(exception_NoInit);
-        if ( this.getCycleState() != PlayerFunction.REQUEST ) throw new Exception(exception_CycleRequest);
+        if ( this.cycleState == PlayerFunction.NULL ) throw new Exception(exception_NoInit);
+        if ( this.cycleState != PlayerFunction.REQUEST ) throw new Exception(exception_CycleRequest);
 
         Move move = requestMove();
 
@@ -101,10 +130,17 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
         return move;
     }
 
+    /**
+     * Fordert einen Zug vom Spieler an, wie der {@link Move} angefordert wird, wird der jeweiligen Implementation der
+     * abstrakten Klasse ueberlassen.
+     *
+     * @return Der vom Spieler zurueckgegebene Zug.
+     * @throws Exception       Falls der jeweilige Spieler keinen Zug angeben konnte.
+     * @throws RemoteException Falls ein Fehler waehrend der Netzwerk-Kommunikation aufgetreten ist
+     */
+    /* INFO: Method is abstract because requesting a move from the player works differently with each implementation */
     protected abstract Move requestMove() throws Exception, RemoteException;
 
-
-    /* TODO: All the things need doing. */
 
     /**
      * Stellt die vom Interface {@link flowerwarspp.preset.Player} geforderte Methode {@link
@@ -123,7 +159,7 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
         if ( this.cycleState != PlayerFunction.CONFIRM ) throw new Exception(exception_CycleConfirm);
 
         // Verify that player's status and main program's status are equal
-        Status playerBoardState = this.viewer.getStatus();
+        Status playerBoardState = this.boardViewer.getStatus();
 
         if ( ! playerBoardState.equals(status) ) throw new Exception(exception_StatusError);
 
@@ -131,11 +167,9 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
         this.cycleState = PlayerFunction.UPDATE;
     }
 
-    /* TODO: Any and all of this. */
-
     /**
      * Stellt die vom Interface {@link flowerwarspp.preset.Player} geforderte Methode {@link
-     * flowerwarspp.preset.Player#confirm(Status)} bereit.
+     * flowerwarspp.preset.Player#update(Move, Status)} bereit.
      *
      * @param opponentMove Zug des Gegenspielers
      * @param status       Status des Spielbretts des Hauptprogramms nach Ausfuehren des Zuges des Gegenspielers
@@ -154,7 +188,7 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
         this.board.make(opponentMove);
 
         // Verify the status
-        Status playerBoardStatus = this.viewer.getStatus();
+        Status playerBoardStatus = this.boardViewer.getStatus();
 
         if ( ! playerBoardStatus.equals(status) ) throw new Exception(exception_StatusError);
 
@@ -162,7 +196,6 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
         this.cycleState = PlayerFunction.REQUEST;
     }
 
-    /* TODO: Properly handle beginning a new gme (i.e. calling init() in the middle of a running game) */
 
     /**
      * Initialisiert einen Spieler, indem dieser mit einem neuen Spielbrett passender Groesse und der gewuenschten Farbe
@@ -174,6 +207,7 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
      * @throws Exception       Falls waehrend der Initialisierung ein Fehler auftrat
      * @throws RemoteException Falls ein Fehler waehrend der Netzwerk-Kommunikation aufgetreten ist
      */
+    /* TODO: Properly handle beginning a new game (i.e. calling init() in the middle of a running game) */
     @Override
     public void init( int boardSize, PlayerColor playerColour ) throws Exception, RemoteException {
 
@@ -184,7 +218,7 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
 
             // FIXME: Replace DummyBoard with working implementation of MainBoard (or equivalent)
             this.board = new MainBoard(boardSize);
-            this.viewer = new BoardViewer(board);
+            this.boardViewer = board.viewer();
         } else {
 
             // Here we need to handle ending the current game and starting a new one, some form of feedback would be
@@ -212,13 +246,5 @@ abstract class BasePlayer implements flowerwarspp.preset.Player {
      */
     public PlayerColor getPlayerColour() {
         return this.playerColour;
-    }
-
-    protected PlayerFunction getCycleState () {
-        return this.cycleState;
-    }
-
-    protected Collection<Move> getPossibleMoves () {
-        return viewer.getPossibleMoves();
     }
 }
