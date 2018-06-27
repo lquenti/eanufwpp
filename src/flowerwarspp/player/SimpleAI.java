6efa68fa1289e35b60cc52daace63531473dbe6f
Mokka <mokkamichi.99@gmail.com>
2018-06-28 00:42:23 +0200

    SimpleAI sollte auch verwendbar sein

--- src/flowerwarspp/player/SimpleAI.java
+++ src/flowerwarspp/player/SimpleAI.java
@@ -1,16 +1,26 @@
 package flowerwarspp.player;
 
-import flowerwarspp.preset.Move;
-import flowerwarspp.preset.MoveType;
-import flowerwarspp.preset.Player;
-import flowerwarspp.preset.PlayerType;
+import flowerwarspp.board.MainBoard;
+import flowerwarspp.preset.*;
 
 import java.rmi.RemoteException;
+import java.util.LinkedList;
 
+/**
+ * Implementiert die abstrakte Klasse {@link BasePlayer} mit einer simplen KI. Diese KI bedient sich einer limitierten
+ * Bewertungsstrategie zur Auswahl eines Zuges und gibt diesen auf Anforderung zurueck.
+ *
+ * @author Mokka
+ */
 public class SimpleAI extends BasePlayer {
 
+    /**
+     * Eine vordefinierte Nachricht einer {@link Exception}, welche geworfen wird, wenn die einfache KI mit ihrer
+     * Strategie keinen Zug auswaehlen konnte.
+     */
     private static final String exception_NoMove =
             "Die einfache KI konnte keinen Zug auswaehlen.";
+
 
     /**
      * Fordert einen Zug an, nach den Vorgaben der Interface-Methode {@link Player#request()}. Diese Methode bedient
@@ -24,13 +34,40 @@ public class SimpleAI extends BasePlayer {
     // TODO: Implement the actual weighted algorithm
     protected Move requestMove() throws Exception, RemoteException {
 
-        for ( Move move :
-                this.getPossibleMoves() ) {
-            if (!move.getType().equals(MoveType.Flower)) continue;
+        int highestScore = 0;
+        Move highestScoredMove = null;
 
-            // TODO: Check if the neighbouring fields have plants for both flowers
+        // Iterate through all the possible moves...
+        for ( Move move :
+                this.boardViewer.getPossibleMoves() ) {
+
+            // We are only concerned with moves that actually make flowers.
+            if ( ! move.getType().equals(MoveType.Flower) ) continue;
+
+            // Obtain the direct neighbours of both flowers.
+            // INFO: Checking if these flowers have been planted or no sounds sane. Have to clarify course of actions here...
+            LinkedList<Flower> firstFlowerNeighbors = ( (MainBoard) board ).getDirectNeighbours(move.getFirstFlower());
+            LinkedList<Flower> secondFlowerNeighbors = ( (MainBoard) board ).getDirectNeighbours(move.getSecondFlower());
+
+            // Calculate the score as indicated by the strategy.
+            int score = ( firstFlowerNeighbors.size() + 1 ) * ( secondFlowerNeighbors.size() + 1 );
+
+            // If both flowers are attached (i.e. if they're neighbours) double the score.
+            if ( firstFlowerNeighbors.contains(move.getSecondFlower()) )
+                score *= 2;
+
+            // If the score of the currently observed move is higher than the previously highest score, update the relevant variables.
+            if ( score > highestScore ) {
+                highestScore = score;
+                highestScoredMove = move;
+            }
         }
 
-        return null;
+        // If we do not have a highest scored move, we throw.
+        if ( highestScoredMove == null )
+            throw new Exception(exception_NoMove);
+
+        return highestScoredMove;
     }
+
 }
