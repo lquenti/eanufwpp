76057eb0f1f281d33a0e522b7ebddcb914ddf6db
Thilo Wischmeyer <thilo.wischmeyer@stud.uni-goettingen.de>
2018-06-29 22:47:26 +0200

    Delay einbauen (fixes #17)

--- src/flowerwarspp/Main.java
+++ src/flowerwarspp/Main.java
@@ -10,13 +10,15 @@ public class Main {
 	private static int boardSize;
 	private static PlayerType redType;
 	private static PlayerType blueType;
+	private static int delay;
 
 	private static void quitWithUsage() {
 		System.out.println("Verwendung:");
-		System.out.println("flowerwarspp.Main -size <Spielfeldgröße> -red <Spielertyp> -blue <Spielertyp>");
+		System.out.println("flowerwarspp.Main -size <Spielfeldgröße> -red <Spielertyp> -blue <Spielertyp> -delay <Verzögerung>");
 		System.out.println();
 		System.out.println("Spielfeldgröße: Zahl zwischen 3 und 30");
 		System.out.println("Spielertyp:     \"human\", \"random\" oder \"simple\"");
+		System.out.println("Verzögerung:    Zeit zwischen Zügen in Millisekunden");
 		System.exit(1);
 	}
 
@@ -35,11 +37,12 @@ public class Main {
 			boardSize = argumentParser.getSize();
 			redType = argumentParser.getRed();
 			blueType = argumentParser.getBlue();
+			delay = argumentParser.getDelay();
 		} catch (ArgumentParserException e) {
 			quitWithUsage();
 		}
 
-		if (boardSize < 3 || boardSize > 30) {
+		if (boardSize < 3 || boardSize > 30 || delay < 0) {
 			quitWithUsage();
 		}
 
@@ -65,6 +68,8 @@ public class Main {
 				Player t = currentPlayer;
 				currentPlayer = oppositePlayer;
 				oppositePlayer = t;
+
+				Thread.sleep(delay);
 			}
 		} catch (Exception e) {
 			System.out.println("Ein Fehler ist aufgetreten:");
