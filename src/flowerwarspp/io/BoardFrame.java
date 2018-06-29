305de49212e3d9e1457fe807e7c81df1296d5410
Thilo Wischmeyer <thilo.wischmeyer@stud.uni-goettingen.de>
2018-06-29 15:04:18 +0200

    Testknöpfe aus Frame entfernen.

--- src/flowerwarspp/io/BoardFrame.java
+++ src/flowerwarspp/io/BoardFrame.java
@@ -12,10 +12,6 @@ import java.util.stream.Collectors;
 public class BoardFrame extends JFrame implements Requestable, Output {
 	private Viewer viewer;
 
-	private SpringLayout springLayout = new SpringLayout();
-	private JPanel container = new JPanel(this.springLayout);
-	private JButton testFlowerMove = new JButton("Random flower");
-	private JButton testDitchMove = new JButton("Random ditch");
 	private BoardDisplay boardDisplay = new BoardDisplay();
 
 	public BoardFrame(Viewer viewer) {
@@ -25,20 +21,13 @@ public class BoardFrame extends JFrame implements Requestable, Output {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(600, 600);
 
-		this.add(this.container);
-		this.container.add(this.boardDisplay);
-		this.container.add(this.testFlowerMove);
-		this.container.add(this.testDitchMove);
+		this.add(this.boardDisplay);
 		this.boardDisplay.updateSize();
 
 		this.boardDisplay.addMouseListener((MouseClickListener) e -> {
 			this.setViewer(this.viewer);
 			System.out.println("Hallo");
 		});
-
-		// Call after adding all the components to the container.
-		// Sets up the layout of everything.
-		this.setupConstraints();
 
 		// TODO: Make this obsolete.
 		this.boardDisplay.setBorder(new BevelBorder(BevelBorder.LOWERED));
@@ -50,37 +39,6 @@ public class BoardFrame extends JFrame implements Requestable, Output {
 		this.invalidate();
 		this.repaint();
 		this.setVisible(true);
-	}
-
-	/**
-	 * Erstelle {@link SpringLayout.Constraints} für die einzelnen Elemente des {@link JFrame}s.
-	 * Diese Constraints legen fest, wie die Elemente in Relation zum Fenster bzw. zueinander liegen
-	 * sollen.
-	 */
-	private void setupConstraints() {
-		// Lays out the display itself
-		this.springLayout.putConstraint(
-		        SpringLayout.HORIZONTAL_CENTER, this.boardDisplay, 0,
-		        SpringLayout.HORIZONTAL_CENTER, this.container);
-		this.springLayout.putConstraint(
-		        SpringLayout.VERTICAL_CENTER, this.boardDisplay, 0,
-		        SpringLayout.VERTICAL_CENTER, this.container);
-
-		// Lays out the flower move test button
-		this.springLayout.putConstraint(
-		        SpringLayout.SOUTH, this.testFlowerMove, 0,
-				SpringLayout.SOUTH, this.container);
-		this.springLayout.putConstraint(
-		        SpringLayout.WEST, this.testFlowerMove, 0,
-		        SpringLayout.WEST, this.container);
-
-		// Lays out the ditch move test button
-		this.springLayout.putConstraint(
-		        SpringLayout.SOUTH, this.testDitchMove, 0,
-		        SpringLayout.SOUTH, this.container);
-		this.springLayout.putConstraint(
-		        SpringLayout.WEST, this.testDitchMove, 0,
-		        SpringLayout.EAST, this.testFlowerMove);
 	}
 
 	public void setViewer(Viewer viewer) {
