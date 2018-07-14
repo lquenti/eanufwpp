bba09f3af1bd3cc2722defa63046d63dc41c8157
Michael Merse <michaelotto.merse@stud.uni-goettingen.de>
2018-07-14 17:11:06 +0200

    KIs angepasst

--- src/flowerwarspp/player/AdvancedAI1.java
+++ src/flowerwarspp/player/AdvancedAI1.java
@@ -11,10 +11,42 @@ import java.util.Collection;
  * Diese Klasse stellt einen Computerspieler mit verbesserter Strategie (Level 1) zur Verfügung.
  * <p>
  * Der zu spielende Zug wird auf Basis eines angepassten Bewertungsalgorithmus des simplen Computerspielers ausgewählt.
- *
- *
  */
 public class AdvancedAI1 extends BaseAI {
+
+	/**
+	 * Daten-Struct zum Speichern und Verarbeiten von Zugbewertungen. Da nur Daten transportiert werden, und keine
+	 * Operationen in Form von Methoden von dieser Klasse bereitgestellt werden, sind die Instanzvariablen der Klasse
+	 * <code>package-private</code>.
+	 */
+	private class ScoreResults {
+		/**
+		 * Bewertet das Bilden von zusammenhängenden Blumenstrukturen (also von Beeten und Gärten).
+		 */
+		int gardenScore = 0;
+
+		/**
+		 * Bewertet das Setzen von Zügen in der Nähe von Blumen die keinem Spieler gehören.
+		 */
+		int notOwnedFlowerScore = 0;
+	}
+
+	/**
+	 * Beschreibt den Faktor, mit welchem die Bewertung von Zügen, die zusammenhängende Beete bilden, multipliziert
+	 * werden soll (erstes Element des Rückgabearrays der Methode {@link #getNeighborScore(Collection)}).
+	 */
+	private static final int gardenMultiplier = 8;
+
+	/**
+	 * Beschreibt den Faktor, mit welchem die Bewertung von Zügen, die Blumen in der Nähe des Gegners anordnen,
+	 * multipliziert werden soll (zweites Element des Rückgabearrays der Methode {@link #getNeighborScore(Collection)}.
+	 */
+	private static final int notOwnedFlowerMultiplier = 1;
+
+	/**
+	 * Beschreibt den Faktor, mit welchem Züge, die Blumen in Paaren anordnen, multipliziert werden sollen.
+	 */
+	private static final int flowerPairMultiplier = 2;
 
 	/**
 	 * Konstruktor, um eine neue Instanz dieser Klasse zu erstellen.
@@ -41,17 +73,18 @@ public class AdvancedAI1 extends BaseAI {
 				final Collection<Flower> secondFlowerNeighbors = boardViewer.getDirectNeighbors(move.getSecondFlower());
 
 				// Die Nachbarwertung für zusammenhängende Beete und Cluster berechnen.
-				final int[] s1 = getNeighborScore(firstFlowerNeighbors);
-				final int[] s2 = getNeighborScore(secondFlowerNeighbors);
+				final ScoreResults s1 = getNeighborScore(firstFlowerNeighbors);
+				final ScoreResults s2 = getNeighborScore(secondFlowerNeighbors);
 
 				// Züge, die Beete oder Gärten schaffen, werden priorisiert, Züge die in der Nähe des Gegners liegen
 				// werden negativ bewertet.
-				int score = 4 * ( s1[0] + 1 ) * ( s2[0] + 1 ) - ( s1[1] + 1 ) * ( s2[1] + 1 );
+				int score = gardenMultiplier * ( s1.gardenScore + 1 ) * ( s2.gardenScore + 1 )
+						+ notOwnedFlowerMultiplier * ( s1.notOwnedFlowerScore + 1 ) * ( s2.notOwnedFlowerScore + 1 );
 
 				// Falls die beiden zu setzenden Blumen nebeneinander liegen, soll der Score verdoppelt werden.
 				// So werden Züge mit einzeln gesetzten Blumen immer nur dann gemacht, wenn es nicht anders geht.
 				if (firstFlowerNeighbors.contains(move.getSecondFlower()))
-					score *= 2;
+					score *= flowerPairMultiplier;
 
 				return score;
 
@@ -85,27 +118,26 @@ public class AdvancedAI1 extends BaseAI {
 
 	/**
 	 * Berechnet den Score für die Nachbarblumen beider Blumen eines Flower-Moves. Die Berechnung läuft für beide
-	 * Nachbarn glech, deshalb wurde sie in diese Methode ausgelagert.
+	 * Nachbarn gleich, deshalb wurde sie in diese Methode ausgelagert.
 	 *
 	 * @param flowerNeighbors Die Nachbarn einer Blume, dessen Score berechnet werden soll
 	 * @return Der Score basierend auf den Nachbarn einer Blume
 	 */
-	private int[] getNeighborScore(Collection<Flower> flowerNeighbors) {
-		// Der Rückgabewert ist ein Array der Größe 2.
-		int[] res = new int[] {0,0};
+	private ScoreResults getNeighborScore(Collection<Flower> flowerNeighbors) {
+		// Der Rückgabewert ist ein ScoreResults Objekt.
+		ScoreResults result = new ScoreResults();
 
-		// Durch die direkten Nachbarn der betrachteten Blume iterieren und das erste Element des Rückgabearrays immer
-		// dann inkrementieren, wenn einer dieser direkten Nachbarn der eigenen Farbe gehört.
+		// Durch die direkten Nachbarn der betrachteten Blume iterieren und gardenScore immer genau dann
+		// inkrementieren, wenn einer dieser direkten Nachbarn der eigenen Farbe gehört.
 		// So werden Beete und Gärten gebildet.
-		// Nachbarn, welche dem Gegner gehören, inkremementieren das zweite Element des Arrays.
+		// Nachbarn, welche keinem Spieler gehören, inkremementieren notOwnedFlowerScore.
 		for (Flower neighbor : flowerNeighbors) {
 			if (boardViewer.getFlowerColor(neighbor) == getPlayerColor())
-				res[0]++;
+				result.gardenScore++;
+			else if (boardViewer.getFlowerColor(neighbor) == null)
+				result.notOwnedFlowerScore++;
 
-			if (boardViewer.getFlowerColor(neighbor) == ( getPlayerColor() == PlayerColor.Red ? PlayerColor.Blue
-					: PlayerColor.Red ))
-				res[1]++;
 		}
-		return res;
+		return result;
 	}
 }
--- src/flowerwarspp/player/AdvancedAI2.java
+++ src/flowerwarspp/player/AdvancedAI2.java
@@ -12,10 +12,59 @@ import java.util.Collection;
  * <p>
  * Der zu spielende Zug wird auf Basis eines angepassten Bewertungsalgorithmus des verbesserten Computerspielers (Level
  * 1) ausgewählt.
- *
- *
  */
 public class AdvancedAI2 extends BaseAI {
+
+	/**
+	 * Daten-Struct zum Speichern und Verarbeiten von Zugbewertungen. Da nur Daten transportiert werden, und keine
+	 * Operationen in Form von Methoden von dieser Klasse bereitgestellt werden, sind die Instanzvariablen der Klasse
+	 * <code>package-private</code>.
+	 */
+	private class ScoreResults {
+		/**
+		 * Bewertet das Bilden von zusammenhängenden Blumenstrukturen (also von Beeten und Gärten).
+		 */
+		int gardenScore = 0;
+
+		/**
+		 * Bewertet das Setzen von Zügen in der Nähe von Blumen die keinem Spieler gehören.
+		 */
+		int notOwnedFlowerScore = 0;
+
+		/**
+		 * Bewertet das Setzen von Blumen in direkter Nähe zu bestehenden Blumen.
+		 */
+		int clusterScore = 0;
+	}
+
+	/**
+	 * Beschreibt den zur Berechnung der Nummer des Zugs ab welchem Cluster bebildet werden sollen benutzten Divisor
+	 * der {@link #init(int, PlayerColor)}-Methode.
+	 */
+	private static final int clusterStartDivisor = 15;
+
+	/**
+	 * Beschreibt den Faktor, mit welchem die Bewertung von Zügen die zusammenhängende Beete bilden multipliziert
+	 * werden soll (erstes Element des Rückgabearrays der Methode {@link #getNeighborScore(Flower)}).
+	 */
+	private static final int gardenMultiplier = 20;
+
+	/**
+	 * Beschreibt den Faktor, mit welchem die Bewertung von Zügen, die Blumen in der Nähe des Gegners anordnen,
+	 * multipliziert werden soll (zweites Element des Rückgabearrays der Methode {@link #getNeighborScore(Flower)}.
+	 */
+	private static final int notOwnedFlowerMultiplier = 1;
+
+	/**
+	 * Beschreibt den Faktor, mit welchem die Bewertung von Zügen die Blumen(-beete) in Clustern anordnen
+	 * multipliziert werden soll (zweites Element des Rückgabearrays der Methode {@link #getNeighborScore(Flower)}.
+	 */
+	private static final int clusterMultiplier = 1;
+
+	/**
+	 * Beschreibt die Konstante, welche auf Züge die Blumen in Paaren anordnen aufaddiert werden soll.
+	 */
+	private static final int flowerPairSummand = 15;
 
 	/**
 	 * Der Zug, ab welchem Cluster gebildet werden sollen.
@@ -58,7 +107,7 @@ public class AdvancedAI2 extends BaseAI {
 		if (!boardViewer.getFlowers(getPlayerColor()).isEmpty())
 			startClusteringAt = 0;
 		else
-			startClusteringAt = ( boardSize * boardSize ) / 20;
+			startClusteringAt = ( boardSize * boardSize ) / clusterStartDivisor;
 
 		moveNr = 0;
 	}
@@ -79,17 +128,19 @@ public class AdvancedAI2 extends BaseAI {
 				final Collection<Flower> firstFlowerNeighbors = boardViewer.getDirectNeighbors(move.getFirstFlower());
 
 				// Die Nachbarwertung für zusammenhängende Beete und Cluster berechnen.
-				final int[] s1 = getNeighborScore(move.getFirstFlower());
-				final int[] s2 = getNeighborScore(move.getSecondFlower());
+				final ScoreResults s1 = getNeighborScore(move.getFirstFlower());
+				final ScoreResults s2 = getNeighborScore(move.getSecondFlower());
 
 				// Züge, die Beete oder Gärten schaffen, werden priorisiert, Züge die Cluster bilden haben geringere
 				// Priorität.
-				int score = 20 * ( s1[0] + 1 ) * ( s2[0] + 1 ) + ( s1[1] + 1 ) * ( s2[1] + 1 );
+				int score = gardenMultiplier * ( s1.gardenScore + 1 ) * ( s2.gardenScore + 1 )
+						+ notOwnedFlowerMultiplier * ( s1.notOwnedFlowerScore + 1 ) * ( s2.notOwnedFlowerScore + 1 )
+						+ clusterMultiplier * ( s1.clusterScore + 1 ) * ( s2.clusterScore + 1 );
 
 				// Falls die beiden zu setzenden Blumen nebeneinander liegen, soll der Score verdoppelt werden.
 				// So werden Züge mit einzeln gesetzten Blumen immer nur dann gemacht, wenn es nicht anders geht.
 				if (firstFlowerNeighbors.contains(move.getSecondFlower())) {
-					score += 15;
+					score += flowerPairSummand;
 				}
 
 				return score;
@@ -128,27 +179,31 @@ public class AdvancedAI2 extends BaseAI {
 	 *
 	 * @param flower Die Blume, dessen Score berechnet werden soll
 	 * @return Der Score basierend auf den Nachbarn einer Blume. Das erste Element des Arrays beschreibt den Score für
-	 * zusammenhängende Blumen, das zweite Element beschreibt den Score für das Clustering.
+	 * zusammenhängende Blumen, das zweite Element beschreibt den Score für Blumen, die keinem Spieler gehören und das
+	 * dritte Element beschreibt den Score für das Clustering.
 	 */
-	private int[] getNeighborScore(Flower flower) {
-		// Der Rückgabewert ist ein Array der Größe 2.
-		int[] res = new int[]{ 0, 0 };
+	private ScoreResults getNeighborScore(Flower flower) {
+		// Der Rückgabewert ist ein Objekt der Klasee ScoreResults.
+		ScoreResults result = new ScoreResults();
 
-		// Durch die direkten Nachbarn der betrachteten Blume iterieren und das erste Element des Rückgabearrays immer
-		// dann inkrementieren, wenn einer dieser direkten Nachbarn der eigenen Farbe gehört.
+		// Durch die direkten Nachbarn der betrachteten Blume iterieren und gardenScore immer dann inkrementieren,
+		// wenn einer dieser direkten Nachbarn der eigenen Farbe gehört.
 		// So werden Beete und Gärten gebildet.
+		// Außerdem wird notOwnedFlowerScore immer dann inkrementiert, wenn der betrachtete Nachbar noch keinem
+		// Spieler gehört.
 		for (Flower neighbor : boardViewer.getDirectNeighbors(flower)) {
 			if (boardViewer.getFlowerColor(neighbor) == getPlayerColor())
-				res[0]++;
+				result.gardenScore++;
+			else if (boardViewer.getFlowerColor(neighbor) == null)
+				result.notOwnedFlowerScore++;
 		}
 
 		// Falls ein Zug, welcher ein bestehendes Blumenbeet erweitert, gemacht werden kann, oder falls Beete noch nicht
 		// in Clustern angeordnet werden sollen, wird hier die Methode verlassen.
-		if (res[0] > 0 || moveNr <= startClusteringAt) return res;
+		if (result.gardenScore > 0 || moveNr <= startClusteringAt) return result;
 
-		// Durch alle Nachbarn der zu betrachtenden Blume iterieren und das zweite Element des Rückgabearrays immer dann
-		// inkrementieren, wenn einer der direkten Nachbarn des aktuell betrachteten Nachbars schon diesem Spieler
-		// gehört.
+		// Durch alle Nachbarn der zu betrachtenden Blume iterieren und clusterScore immer dann inkrementieren, wenn
+		// einer der direkten Nachbarn des aktuell betrachteten Nachbars schon diesem Spieler gehört.
 		// Ist bei den Nachbarn der betrachteten Blume flower eine Blume dabei, welche diesem Spieler bereits gehört,
 		// dann wird die Methode sofort verlassen.
 		// Dadurch werden Blumen in der Nähe von bestehenden Beeten gesetzt, sodass sie sich mit Ditches verbinden
@@ -156,15 +211,15 @@ public class AdvancedAI2 extends BaseAI {
 		for (Flower neighbor : boardViewer.getAllNeighbors(flower)) {
 
 			if (boardViewer.getFlowerColor(neighbor) == getPlayerColor()) {
-				res[1] = - 2;
-				return res;
+				result.clusterScore = - 2;
+				return result;
 			}
 
 			for (Flower f1 : boardViewer.getDirectNeighbors(neighbor)) {
 				if (boardViewer.getFlowerColor(f1) == getPlayerColor())
-					res[1]++;
+					result.clusterScore++;
 			}
 		}
-		return res;
+		return result;
 	}
 }
